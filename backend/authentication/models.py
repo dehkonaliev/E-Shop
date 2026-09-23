ca737767a4e99b16db1437191d502f7a9a6d3963
Maqsudbek <dehkanaliev@gmail.com>
2026-09-23 07:58:03 +0500

    Authentications models

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from baseapp.models import BaseModel
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(BaseModel, AbstractUser):
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    
+    
+    
